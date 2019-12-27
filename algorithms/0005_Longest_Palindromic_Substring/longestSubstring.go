2dfce1fa97da20cca82be2ff01f99880efd781cf
xiejian <xiejian>
2019-12-27 15:46:37 +0800

    005

--- algorithms/0005_Longest_Palindromic_Substring/longestSubstring.go
+++ algorithms/0005_Longest_Palindromic_Substring/longestSubstring.go
@@ -23,7 +23,23 @@ package _005_Longest_Palindromic_Substring
 
 //leetcode submit region begin(Prohibit modification and deletion)
 func longestPalindrome(s string) string {
-
+	if len(s) < 2 {
+		return s
+	}
+	// P[i][j]表示回文，P[i+1][j-1]也是回文
+	longest := s[0:1]
+	for i := 1; i < len(s); i++ {
+		for rightStep := 0; rightStep < 2; rightStep++ {
+			for p, q := i-1, i+rightStep; p >= 0 && q < len(s) && s[p] == s[q]; {
+				if q-p+1 > len(longest) {
+					longest = s[p : q+1]
+				}
+				p--
+				q++
+			}
+		}
+	}
+	return longest
 }
 
 // 查找最长的回文字符串
