5da0e648ae2dd18454ce16d05748f6656a784534
xiejian <xiejian>
2019-12-27 10:43:42 +0800

    004

---
+++ algorithms/0005_Longest_Palindromic_Substring/longestSubstring.go
@@ -0,0 +1,29 @@
+/**
+ *Created by Xie Jian on 2019/12/18 15:20
+ */
+package _005_Longest_Palindromic_Substring
+
+//Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+//
+// Example 1:
+//
+//
+//Input: "babad"
+//Output: "bab"
+//Note: "aba" is also a valid answer.
+//
+//
+// Example 2:
+//
+//
+//Input: "cbbd"
+//Output: "bb"
+//
+// Related Topics String Dynamic Programming
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func longestPalindrome(s string) string {
+
+}
+
+// 查找最长的回文字符串
