3b294c761cbd21f9e2e2b51a2b72b92483954cf4
xiejian <xiejian>
2020-01-15 10:07:17 +0800

    完成0006题

--- algorithms/0006_ZigZag_Conversion/zigZagConversion.go
+++ algorithms/0006_ZigZag_Conversion/zigZagConversion.go
@@ -3,6 +3,8 @@
  */
 package _006_ZigZag_Conversion
 
+import "strings"
+
 //The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
 //
 //
@@ -43,7 +45,17 @@ func convert(s string, numRows int) string {
 	if numRows == 1 {
 		return s
 	}
-
+	var ret = make([]string, numRows)
+	var T = 2*numRows - 2 // 周期（必定为偶数）
+	for index, bt := range []byte(s) {
+		rows := index % T
+		if rows > T/2 {
+			ret[T-rows] += string(bt)
+		} else {
+			ret[rows] += string(bt)
+		}
+	}
+	return strings.Join(ret, "")
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
