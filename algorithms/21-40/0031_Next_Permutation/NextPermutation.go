39a8f2f6e915d108dc98268057787aacfc2ff2c2
xiejian <xiejian>
2020-03-04 14:23:10 +0800

    补全代码

--- algorithms/21-40/0031_Next_Permutation/NextPermutation.go
+++ algorithms/21-40/0031_Next_Permutation/NextPermutation.go
@@ -18,5 +18,29 @@ package _031_Next_Permutation
 
 //leetcode submit region begin(Prohibit modification and deletion)
 func nextPermutation(nums []int) {
+	if len(nums) <= 1 {
+		return
+	}
 
+	i, j, k := len(nums)-2, len(nums)-1, len(nums)-1
+
+	// find: A[i]<A[j]
+	for i >= 0 && nums[i] >= nums[j] {
+		i--
+		j--
+	}
+
+	if i >= 0 { // 不是最后一个排列
+		// find: A[i]<A[k]
+		for nums[i] >= nums[k] {
+			k--
+		}
+		// swap A[i], A[k]
+		nums[i], nums[k] = nums[k], nums[i]
+	}
+
+	// reverse A[j:end]
+	for i, j := j, len(nums)-1; i < j; i, j = i+1, j-1 {
+		nums[i], nums[j] = nums[j], nums[i]
+	}
 }
