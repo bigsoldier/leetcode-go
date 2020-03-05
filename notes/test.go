08ecb9aed0267197da80346d016f6c0456ef9b3d
xiejian <xiejian>
2020-03-05 16:46:23 +0800

    面试题完成12题

--- notes/test.go
+++ notes/test.go
@@ -2,30 +2,12 @@ package main
 
 import "fmt"
 
+type MyInt1 int
+type MyInt2 = int
+
 func main() {
-	sn1 := struct {
-		age  int
-		name string
-	}{age: 11, name: "qq"}
-	sn2 := struct {
-		age  int
-		name string
-	}{age: 11, name: "qq"}
-
-	if sn1 == sn2 {
-		fmt.Println("sn1 == sn2")
-	}
-
-	sm1 := struct {
-		age int
-		m   map[string]string
-	}{age: 11, m: map[string]string{"a": "1"}}
-	sm2 := struct {
-		age int
-		m   map[string]string
-	}{age: 11, m: map[string]string{"a": "1"}}
-
-	if sm1 == sm2 {
-		fmt.Println("sm1 == sm2")
-	}
+	var i int = 0
+	var i1 MyInt1 = i
+	var i2 MyInt2 = i
+	fmt.Println(i1, i2)
 }
