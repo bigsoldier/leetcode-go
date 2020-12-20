e57f84de575bdfac65887731ff2b2371622f7625
betterfor <1697606384@qq.com>
2020-12-20 21:50:55 +0800

    完成316题

---
+++ algorithms/0000passed/00316.remove-duplicate-letters/去除重复字母.go
@@ -0,0 +1,27 @@
+package code
+
+func removeDuplicateLetters(s string) string {
+	var left [26]int
+	for _, ch := range s {
+		left[ch-'a']++
+	}
+	var stack []byte
+	var inStack [26]bool
+	for i := range s {
+		ch := s[i]
+		if !inStack[ch-'a'] {
+			for len(stack) > 0 && ch < stack[len(stack)-1] {
+				last := stack[len(stack)-1] - 'a'
+				if left[last] == 0 {
+					break
+				}
+				stack = stack[:len(stack)-1]
+				inStack[last] = false
+			}
+			stack = append(stack, ch)
+			inStack[ch-'a'] = true
+		}
+		left[ch-'a']--
+	}
+	return string(stack)
+}
