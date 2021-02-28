18148cbd520385fe6d9077fadf2df51df74b725f
betterfor <1697606384@qq.com>
2021-02-28 18:30:49 +0800

    完成896题(数组)

---
+++ algorithms/0000passed/00932.monotonic-array/单调数列.go
@@ -0,0 +1,14 @@
+package code
+
+func isMonotonic(A []int) bool {
+	var inc, dec = true, true
+	for i := 0; i < len(A)-1; i++ {
+		if A[i] > A[i+1] {
+			inc = false
+		}
+		if A[i] < A[i+1] {
+			dec = false
+		}
+	}
+	return inc || dec
+}
